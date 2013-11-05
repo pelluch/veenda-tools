fba655f4e3382d8091d21ffb924734d57c686a1f
Pablo Lluch <pablo.lluch@gmail.com>
2013-11-05 11:57:53 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VeendaTools::Application.config.secret_token = '5892160e36e8dc7b82e5b1afe6dbcb9c3e34a985ce198377bd5e575793805fbf6d02c312bd5efdebfb2efd641c6e5dab31b0edbca2b2c81691b80bafdad7bb26'
